226dcea9e4384be487251d915a4e2b5c7489cf67
EricYangXD <ericyangxinde@gmail.com>
2018-08-12 22:46:31 +0800

    init

---
+++ app/src/main/java/com/example/ericyang/lbstest/MyLocationListener.java
@@ -0,0 +1,11 @@
+package com.example.ericyang.lbstest;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+
+/**
+ * Created by EricYang on 2018/8/12 0012.
+ */
+
+
